232852a23d81ba99046d019b4b1d838210ad5615
ulisses <iamurise@gmail.com>
2022-08-25 08:05:16 -0300

    initial commit

---
+++ src/main/java/group4/jenkins_example/JenkinsExampleApplication.java
@@ -0,0 +1,13 @@
+package group4.jenkins_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group4/jenkins_example/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package group4.jenkins_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
